c5e3664b458b34940fa1ea9c1ee84a07a5f325ca
deshmukh30 <161029715+deshmukh30@users.noreply.github.com>
2024-03-13 21:50:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,236 +1,210 @@
-const audio6=new Audio("Akhiyaan-Gulaab(PaglaSongs).mp3");
-document.querySelectorAll(".audio6").addEventListener("click",function(){
+document.querySelectorAll(".audio5").addEventListener("click",function(){
 audio6.pause();
  });
-document.querySelectorAll(".audio6").addEventListener("click",function(){
 const audio6=new Audio("Akhiyaan-Gulaab(PaglaSongs).mp3");
+document.querySelectorAll(".audio6").addEventListener("click",function(){
 audio6.play();
  });
-
-const audio7=new Audio("What-Jhumka(PaglaSongs).mp3");
-document.querySelectorAll(".audio7").addEventListener("click",function(){
+document.querySelectorAll(".audio6").addEventListener("click",function(){
  audio7.pause();
  });
- document.querySelectorAll(".audio7").addEventListener("click",function(){
+const audio7=new Audio("What-Jhumka(PaglaSongs).mp3");
+document.querySelectorAll(".audio7").addEventListener("click",function(){
 audio7.play();
- });
-
-const audio8=new Audio("O-Maahi(PaglaSongs).mp3");
-document.querySelectorAll(".audio8").addEventListener("click",function(){
+});
+document.querySelectorAll(".audio7").addEventListener("click",function(){
  audio8.pause();
  });
+const audio8=new Audio("O-Maahi(PaglaSongs).mp3");
 document.querySelectorAll(".audio8").addEventListener("click",function(){
-const audio7=new Audio("What-Jhumka(PaglaSongs).mp3");
 audio8.play();
  });
-
-const audio9=new Audio("Teri-Baaton-Mein-Aisa-Uljha-Jiya-(Title-Track)(PaglaSongs).mp3");
-document.querySelectorAll(".audio9").addEventListener("click",function(){
+document.querySelectorAll(".audio8").addEventListener("click",function(){
  audio9.pause();
  });
+const audio9=new Audio("Teri-Baaton-Mein-Aisa-Uljha-Jiya-(Title-Track)(PaglaSongs).mp3");
  document.querySelectorAll(".audio9").addEventListener("click",function(){
  audio9.play();
  });
-
-const audio10=new Audio("Hass-Hass(PaglaSongs).mp3");
-document.querySelectorAll(".audio10").addEventListener("click",function(){
+document.querySelectorAll(".audio9").addEventListener("click",function(){
  audio10.pause();
  });
+const audio10=new Audio("Hass-Hass(PaglaSongs).mp3");
  document.querySelectorAll(".audio10").addEventListener("click",function(){
  audio10.play();
  });
-
-const audio11=new Audio("Khayaal_-_Zehen_128_Kbps.mp3");
-document.querySelectorAll(".audio11").addEventListener("click",function(){
+document.querySelectorAll(".audio10").addEventListener("click",function(){
  audio11.pause();
  });
+const audio11=new Audio("Khayaal_-_Zehen_128_Kbps.mp3");
  document.querySelectorAll(".audio11").addEventListener("click",function(){
  audio11.play();
  });
-
-const audio12=new Audio("Subhanallah (Yeh Jawaani Hai Deewani)-(PagalWorld.Ink).mp3");
-document.querySelectorAll(".audio12").addEventListener("click",function(){
+document.querySelectorAll(".audio11").addEventListener("click",function(){
  audio12.pause();
  });
+const audio12=new Audio("Subhanallah (Yeh Jawaani Hai Deewani)-(PagalWorld.Ink).mp3");
  document.querySelectorAll(".audio12").addEventListener("click",function(){
  audio12.play();
  });
-
- const audio13=new Audio("Khairiyat-Pucho(PaglaSongs).mp3");
- document.querySelectorAll(".audio13").addEventListener("click",function(){
+ document.querySelectorAll(".audio12").addEventListener("click",function(){
   audio13.pause();
   });
+  const audio13=new Audio("Khairiyat-Pucho(PaglaSongs).mp3");
   document.querySelectorAll(".audio13").addEventListener("click",function(){
   audio13.play();
   });
-
-  const audio14=new Audio("Malang-Sajna(PaglaSongs).mp3");
-document.querySelectorAll(".audio14").addEventListener("click",function(){
+ document.querySelectorAll(".audio13").addEventListener("click",function(){
  audio14.pause();
  });
+ const audio14=new Audio("Malang-Sajna(PaglaSongs).mp3");
  document.querySelectorAll(".audio14").addEventListener("click",function(){
  audio14.play();
  });
-
- const audio15=new Audio("Ranjha(PaglaSongs).mp3");
-document.querySelectorAll(".audio15").addEventListener("click",function(){
+document.querySelectorAll(".audio14").addEventListener("click",function(){
  audio15.pause();
  });
+ const audio15=new Audio("Ranjha(PaglaSongs).mp3");
  document.querySelectorAll(".audio15").addEventListener("click",function(){
  audio15.play();
  });
-
- const audio16=new Audio("Dil_Jhoom_-_Gadar_2_128_Kbps.mp3");
-document.querySelectorAll(".audio16").addEventListener("click",function(){
+document.querySelectorAll(".audio15").addEventListener("click",function(){
  audio16.pause();
  });
+ const audio16=new Audio("Dil_Jhoom_-_Gadar_2_128_Kbps.mp3");
  document.querySelectorAll(".audio16").addEventListener("click",function(){
  audio16.play();
  });
-
- const audio17=new Audio("Tum-Kya-Mile(PaglaSongs).mp3");
-document.querySelectorAll(".audio17").addEventListener("click",function(){
+document.querySelectorAll(".audio16").addEventListener("click",function(){
  audio17.pause();
  });
+const audio17=new Audio("Tum-Kya-Mile(PaglaSongs).mp3");
  document.querySelectorAll(".audio17").addEventListener("click",function(){
  audio17.play();
  });
-
- const audio18=new Audio("Raataan-Lambiyan(PaglaSongs).mp3");
-document.querySelectorAll(".audio18").addEventListener("click",function(){
+document.querySelectorAll(".audio17").addEventListener("click",function(){
  audio18.pause();
  });
+const audio18=new Audio("Raataan-Lambiyan(PaglaSongs).mp3");
  document.querySelectorAll(".audio18").addEventListener("click",function(){
  audio18.play();
  });
-
- const audio19=new Audio("Radha---Jab-Harry-Met-Sejal(PagalWorld).mp3");
-document.querySelectorAll(".audio19").addEventListener("click",function(){
+document.querySelectorAll(".audio18").addEventListener("click",function(){
  audio19.pause();
  });
+const audio19=new Audio("Radha---Jab-Harry-Met-Sejal(PagalWorld).mp3");
  document.querySelectorAll(".audio19").addEventListener("click",function(){
  audio19.play();
  });
-
- const audio20=new Audio("Atak-Gaya(PaglaSongs).mp3");
-document.querySelectorAll(".audio20").addEventListener("click",function(){
+document.querySelectorAll(".audio19").addEventListener("click",function(){
  audio20.pause();
  });
+const audio20=new Audio("Atak-Gaya(PaglaSongs).mp3");
  document.querySelectorAll(".audio20").addEventListener("click",function(){
  audio20.play();
  });
-
- const audio31=new Audio("Until-I-Found-You(PaglaSongs).mp3");
-document.querySelectorAll(".audio31").addEventListener("click",function(){
+document.querySelectorAll(".audio30").addEventListener("click",function(){
  audio31.pause();
  });
+ const audio31=new Audio("Until-I-Found-You(PaglaSongs).mp3");
  document.querySelectorAll(".audio31").addEventListener("click",function(){
  audio31.play();
  });
-
- const audio32=new Audio("Harry_Styles_-_Watermelon_Sugar.mp3");
-document.querySelectorAll(".audio32").addEventListener("click",function(){
+document.querySelectorAll(".audio31").addEventListener("click",function(){
  audio32.pause();
  });
+ const audio32=new Audio("Harry_Styles_-_Watermelon_Sugar.mp3");
  document.querySelectorAll(".audio32").addEventListener("click",function(){
  audio32.play();
  });
-
- const audio33=new Audio("Taylor_Swift_-_Cruel_Summer_@BaseNaija.com.mp3");
-document.querySelectorAll(".audio33").addEventListener("click",function(){
+document.querySelectorAll(".audio32").addEventListener("click",function(){
  audio33.pause();
  });
+ const audio33=new Audio("Taylor_Swift_-_Cruel_Summer_@BaseNaija.com.mp3");
  document.querySelectorAll(".audio33").addEventListener("click",function(){
  audio33.play();
  });
-
- const audio34=new Audio("Ruth_B_-_Dandelions_CeeNaija.com_.mp3");
-document.querySelectorAll(".audio34").addEventListener("click",function(){
+document.querySelectorAll(".audio33").addEventListener("click",function(){
  audio34.pause();
  });
+ const audio34=new Audio("Ruth_B_-_Dandelions_CeeNaija.com_.mp3");
  document.querySelectorAll(".audio34").addEventListener("click",function(){
  audio34.play();
  });
-
- const audio35=new Audio("At-My-Worst(PagalNew.Com.Se).mp3");
-document.querySelectorAll(".audio35").addEventListener("click",function(){
+document.querySelectorAll(".audio34").addEventListener("click",function(){
  audio35.pause();
  });
+ const audio35=new Audio("At-My-Worst(PagalNew.Com.Se).mp3");
  document.querySelectorAll(".audio35").addEventListener("click",function(){
  audio35.play();
  });
-
- const audio41=new Audio("Dholida (Loveratri)-(PagalWorld.Ink).mp3");
-document.querySelectorAll(".audio41").addEventListener("click",function(){
+document.querySelectorAll(".audio35").addEventListener("click",function(){
  audio41.pause();
  });
+ const audio41=new Audio("Dholida (Loveratri)-(PagalWorld.Ink).mp3");
  document.querySelectorAll(".audio41").addEventListener("click",function(){
  audio41.play();
  });
-
- const audio42=new Audio("Kamariya 128 Kbps.mp3");
-document.querySelectorAll(".audio42").addEventListener("click",function(){
+document.querySelectorAll(".audio41").addEventListener("click",function(){
  audio42.pause();
  });
+const audio42=new Audio("Kamariya 128 Kbps.mp3");
  document.querySelectorAll(".audio42").addEventListener("click",function(){
  audio42.play();
  });
-
- const audio43=new Audio("Khalasi(PaglaSongs).mp3");
-document.querySelectorAll(".audio43").addEventListener("click",function(){
+document.querySelectorAll(".audio42").addEventListener("click",function(){
  audio43.pause();
  });
+const audio43=new Audio("Khalasi(PaglaSongs).mp3");
  document.querySelectorAll(".audio43").addEventListener("click",function(){
  audio43.play();
  });
-
- const audio44=new Audio("Dhol-Bajaa(PaglaSongs).mp3");
-document.querySelectorAll(".audio44").addEventListener("click",function(){
+document.querySelectorAll(".audio43").addEventListener("click",function(){
  audio44.pause();
  });
+const audio44=new Audio("Dhol-Bajaa(PaglaSongs).mp3");
  document.querySelectorAll(".audio44").addEventListener("click",function(){
  audio44.play();
  });
-
- const audio45=new Audio("Chogada-Tara(PaglaSongs) (1).mp3");
- document.querySelectorAll(".audio45").addEventListener("click",function(){
+ document.querySelectorAll(".audio44").addEventListener("click",function(){
   audio45.pause();
   });
+const audio45=new Audio("Chogada-Tara(PaglaSongs) (1).mp3");
   document.querySelectorAll(".audio45").addEventListener("click",function(){
   audio45.play();
   });
-  const audio46=new Audio("Saathiyaa - Singham_128-(DJMaza).mp3");
- document.querySelectorAll(".audio46").addEventListener("click",function(){
+ document.querySelectorAll(".audio45").addEventListener("click",function(){
   audio46.pause();
   });
+const audio46=new Audio("Saathiyaa - Singham_128-(DJMaza).mp3");
   document.querySelectorAll(".audio46").addEventListener("click",function(){
   audio46.play();
   });
-  const audio47=new Audio("Dholna---Dil-To-Pagal-Hai(PagalWorld).mp3");
- document.querySelectorAll(".audio47").addEventListener("click",function(){
+ document.querySelectorAll(".audio46").addEventListener("click",function(){
   audio47.pause();
   });
+const audio47=new Audio("Dholna---Dil-To-Pagal-Hai(PagalWorld).mp3");
   document.querySelectorAll(".audio47").addEventListener("click",function(){
   audio47.play();
   });
-  const audio48=new Audio("Hum_Tum_-_Hum_Tum_128_Kbps.mp3");
- document.querySelectorAll(".audio48").addEventListener("click",function(){
+ document.querySelectorAll(".audio47").addEventListener("click",function(){
   audio48.pause();
   });
+const audio48=new Audio("Hum_Tum_-_Hum_Tum_128_Kbps.mp3");
   document.querySelectorAll(".audio48").addEventListener("click",function(){
   audio48.play();
   });
-  const audio49=new Audio("Koi-Mil-Gaya(PagalWorld).mp3");
- document.querySelectorAll(".audio49").addEventListener("click",function(){
+ document.querySelectorAll(".audio48").addEventListener("click",function(){
   audio49.pause();
   });
+const audio49=new Audio("Koi-Mil-Gaya(PagalWorld).mp3");
   document.querySelectorAll(".audio49").addEventListener("click",function(){
   audio49.play();
   });
-  const audio50=new Audio("Aankhon-Mein-Teri-Ajab-Si(PaglaSongs).mp3");
- document.querySelectorAll(".audio50").addEventListener("click",function(){
+ document.querySelectorAll(".audio49").addEventListener("click",function(){
   audio50.pause();
   });
+const audio50=new Audio("Aankhon-Mein-Teri-Ajab-Si(PaglaSongs).mp3");
   document.querySelectorAll(".audio50").addEventListener("click",function(){
   audio50.play();
   });
